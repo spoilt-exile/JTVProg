59bc6ecc918c37387d79f7fd9661407a2e2ec626
Stanislav [spoilt_exile] Nepochatov <spoilt.exile@gmail.com>
2011-12-07 22:59:07 +0200

    JTVProg init commit

---
+++ src/jtvprog/JTVProg.java
@@ -0,0 +1,20 @@
+/*
+ * JTVProg is a service software for Ukrainian National Information Agency:
+ * UKRINFORM 2011
+ * v0.1
+ */
+package jtvprog;
+
+/**
+ * Main class
+ * @author Stanislav Nepochatov
+ */
+public class JTVProg {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
